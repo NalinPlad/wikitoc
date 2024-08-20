de2a0af3394a2bde67b7fa94f0e5461b91acb86b
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2024-08-19 23:24:05 -0700

    start button

---
+++ src/lib/StartGame.svelte
@@ -0,0 +1,15 @@
+<input type="button" value="START"/>
+
+<style>
+    input {
+        margin-top: 3rem;
+        padding: 1rem;
+        font-family: 'Times New Roman', Times, serif;
+        font-weight: bold;
+        font-size: 1.5rem;
+        display: block;
+        border-style: double;
+        border-width: 0.5rem;
+        cursor: pointer;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import CategoryButton from '$lib/CategoryButton.svelte';
+    import StartGame from '../lib/StartGame.svelte';
+    import { onDestroy, onMount } from 'svelte';
     import { SeedStore } from '../stores.js';
 
     let seed;
@@ -10,16 +12,17 @@
         seed = value;
     });
 
-    // Cleanup the subscription when the component is destroyed
-    import { onDestroy } from 'svelte';
     onDestroy(() => {
         unsubscribe();
     });
 
-    // Generate a new seed if the current one is null
-    if (seed === null) {
-        SeedStore.set(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
-    }
+    onMount(() => {
+        // Generate a new seed if the current one is null
+        if (seed === null) {
+            const ns = "xxxxx".split('').map(() => Math.floor(Math.random() * 10));
+            SeedStore.set(ns.join(''));
+        }
+    });
 
     // Update the seed in the store if the friendSeed input is changed
     function updateSeed() {
@@ -27,11 +30,11 @@
     }
 </script>
 
-<h1><code>wikitoc</code></h1>
-<h3>Guess Wikipedia articles based on their Tables Of Contents</h3>
+<h1><code>wiki<u>toc</u></code></h1>
+<h3>Guess Countries based on their Wikipedia Table Of Contents</h3>
 <hr/>
 <i>Click PLAY to start. To play along with a friend tell them this <u>seed: {seed}</u>. Or enter your friend's seed below:</i>
 <input type="text" bind:value={friendSeed} placeholder="Enter friend's seed" />
 <button on:click={updateSeed}>Set Seed</button>
 
-<CategoryButton />
+<StartGame />
